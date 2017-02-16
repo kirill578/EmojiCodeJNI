4dfe8b01efdaf5469ebbc6ff1c72c1e00234a01c
Kirill Kulakov <kirill578@gmail.com>
2017-02-16 16:23:48 +0200

    fix

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,9 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := emojicodec
 
 FILE_LIST := $(wildcard $(LOCAL_PATH)/EmojicodeCompiler/*.cpp)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/EmojicodeReal-TimeEngine/ $(LOCAL_PATH)/
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
-LOCAL_CPPFLAGS := -std=c++11 -Wall -fpie -c -fexceptions -frtti -Ofast -iquote . -iquote EmojicodeReal-TimeEngine/ -iquote EmojicodeCompiler/
+LOCAL_CPPFLAGS := -std=c++14 -Wall -fpie -c -fexceptions -frtti -Ofast -iquote . -iquote EmojicodeReal-TimeEngine/ -iquote EmojicodeCompiler/
 LOCAL_LDLIBS := -pie -lm -ldl -rdynamic
 
 include $(BUILD_EXECUTABLE)
