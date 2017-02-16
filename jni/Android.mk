aa88d585398fd6c9d7293236e4c07595b68f9823
Kirill Kulakov <kirill578@gmail.com>
2017-02-16 17:56:25 +0200

    3

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,7 +8,7 @@ FILE_LIST := $(wildcard $(LOCAL_PATH)/EmojicodeCompiler/*.cpp)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/EmojicodeReal-TimeEngine/ $(LOCAL_PATH)/
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
-LOCAL_CPPFLAGS := -std=c++14 -Wall -fpie -c -fexceptions -frtti -Ofast -iquote . -iquote EmojicodeReal-TimeEngine/ -iquote EmojicodeCompiler/
+LOCAL_CPPFLAGS := -std=c++14 -Wall -fpie -c -fexceptions -frtti -Ofast -iquote . -iquote EmojicodeReal-TimeEngine/ -iquote EmojicodeCompiler/ -D__STDC_LIMIT_MACROS=1
 LOCAL_LDLIBS := -pie -lm -ldl -rdynamic
 
 include $(BUILD_EXECUTABLE)
